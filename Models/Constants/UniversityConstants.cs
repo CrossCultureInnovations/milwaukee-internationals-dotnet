2ba1d9487e5039a1d0874d8925cb71fd91606225
Amir <hesamian@uwm.edu>
2024-08-16 15:32:42 -0500

    updated universities

--- Models/Constants/UniversityConstants.cs
+++ Models/Constants/UniversityConstants.cs
@@ -2,5 +2,5 @@
 
 public static class UniversityConstants
 {
-    public static readonly string[] UniversityNames = {"UWM", "Marquette", "MSOE", "Concordia", "Other"};
+    public static readonly string[] UniversityNames = {"UWM", "Marquette", "MSOE", "Concordia", "MCW", "Other"};
 }
